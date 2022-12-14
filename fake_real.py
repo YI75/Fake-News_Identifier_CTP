1293942e4d476d094782efe039deab346e4f4414
Khilola Rustamova <99622577+Rustamova00@users.noreply.github.com>
2022-12-14 17:04:07 +0500

    Update fake_real.py

--- fake_real.py
+++ fake_real.py
@@ -69,5 +69,5 @@ if sentence:
     sentence = vectorizer.transform([sentence])
     prediction = model.predict(sentence)
     prediction_proba = model.predict_proba(sentence)
-    prediction = prediction.replace(r'[','')
+    prediction = prediction.replace(r''\', '')
     st.header(prediction)
