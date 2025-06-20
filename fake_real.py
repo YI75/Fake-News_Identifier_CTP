beceeb3903a05737433ca71fb8a4b6a73c413ca6
YI75 <55035333+YI75@users.noreply.github.com>
2025-06-20 16:31:36 -0400

    Update fake_real.py - changed sklearn to scikit-learn

--- fake_real.py
+++ fake_real.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
+from scikit-learn.feature_extraction.text import TfidfVectorizer
 import nltk
 from nltk.corpus import stopwords
 from nltk import word_tokenize
